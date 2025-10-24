d928029b6c585b4b41271b0a3a4431ac7618f034
Collin Dietz <collin.dietz@geappliances.com>
2025-10-23 21:45:26 -0400

    Fix tests

--- tests/scryfallQueryParser.test.js
+++ tests/scryfallQueryParser.test.js
@@ -3,7 +3,7 @@ const { parseQueryToObject } = require("../src/scryfallQueryParser");
 describe("parseQueryToObject()", () => {
   test("parses basic fields", () => {
     expect(parseQueryToObject("t:dragon c:r")).toEqual({
-      type: ["dragon"],
+      type_line: ["dragon"],
       colors: ["r"],
     });
   });
@@ -22,19 +22,19 @@ describe("parseQueryToObject()", () => {
 
   test("handles multiple same field tokens", () => {
     expect(parseQueryToObject("t:elf t:dragon")).toEqual({
-      type: ["elf", "dragon"],
+      type_line: ["elf", "dragon"],
     });
   });
 
   test("handles negations", () => {
     expect(parseQueryToObject("-t:goblin")).toEqual({
-      type: { include: [], exclude: ["goblin"] },
+      type_line: { include: [], exclude: ["goblin"] },
     });
   });
 
   test("handles combined query", () => {
     expect(parseQueryToObject('t:dragon c:r cmc>3 o:"fire"')).toEqual({
-      type: ["dragon"],
+      type_line: ["dragon"],
       colors: ["r"],
       cmc: { ">": 3 },
       oracle_text: ["fire"],
